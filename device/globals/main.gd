4868a414333276212e6fd5ba24be016a766379e5
Markus Törnqvist <mjt@nysv.org>
2018-02-14 14:11:15 +0200

    Update get_settings for old display

--- device/globals/main.gd
+++ device/globals/main.gd
@@ -120,7 +120,7 @@ func _input(event):
 	# CTRL+F12
 	if (event is InputEventKey and event.pressed and event.control and event.scancode==KEY_F12):
 		OS.print_all_textures_by_size()
-		
+
 	if menu_stack.size() > 0:
 		menu_stack[menu_stack.size() - 1].input(event)
 	elif current != null:
@@ -140,8 +140,8 @@ func _ready():
 #	get_node("/root").set_render_target_clear_on_new_frame(true)
 
 	game_size = Vector2()
-	game_size.x = ProjectSettings.get_setting("display/game_width")
-	game_size.y = ProjectSettings.get_setting("display/game_height")
+	game_size.x = ProjectSettings.get_setting("escoria/display/game_width")
+	game_size.y = ProjectSettings.get_setting("escoria/display/game_height")
 
 	wait_timer = get_node("layers/wait_timer")
 	if wait_timer != null:
